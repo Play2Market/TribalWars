bdb89086907a5a2c885b45b389183248e83eb166
Play2Market <tdj.tiagodejesus@gmail.com>
2025-08-01 19:43:24 -0300

    Add files via upload

---
+++ UserScripts/AutoFarm.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        AutoFarm Console
+// @include     http*://*.tribalwars.*/game.php?*&screen=am_farm*
+// @grant       none
+// @require     https://raw.githubusercontent.com/SeuUser/Repo/main/UserScripts/MinhaAcao/MinhaSeparacao/modal.js
+// @require     https://raw.githubusercontent.com/SeuUser/Repo/main/UserScripts/MinhaAcao/MinhaSeparacao/icone.js
+// ==/UserScript==
