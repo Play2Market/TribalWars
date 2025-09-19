edac0a6f774ae6458febab6acf536327cd2adbf1
Play2Market <tdj.tiagodejesus@gmail.com>
2025-09-19 16:31:29 -0300

    Add files via upload

---
+++ ProjetoK/TabelaRecrutador.js
@@ -0,0 +1,26 @@
+const recruiterTable = `
+<table id="kitsune-recruiter-table" class="kitsune-rec-table-horizontal">
+    <thead>
+        <tr>
+            <th>
+                <div class="kitsune-group-header-content">
+                    <img id="kitsune-manage-groups-btn" src="https://cdn-icons-png.flaticon.com/512/7887/7887701.png" title="Gerenciar Grupos Personalizados">
+                    <span>GRUPOS</span>
+                    <img id="kitsune-group-refresh" src="https://cdn-icons-png.flaticon.com/512/61/61444.png" title="Atualizar Lista de Grupos">
+                </div>
+            </th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_spear.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_sword.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_axe.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_archer.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_spy.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_light.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_marcher.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/unit_heavy.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/recruit/grey/ram.webp"></th>
+            <th><img class="unit-icon" src="https://dsbr.innogamescdn.com/asset/636f8dd3/graphic/unit/recruit/grey/catapult.webp"></th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+`;
